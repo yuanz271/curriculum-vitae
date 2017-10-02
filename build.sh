e8bf3d44a5f21c8adce1235fcaeddf35de8d062e
yuan <yuanz271@gmail.com>
2017-10-02 09:53:29 -0400

    fix: generate pdf in templates

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-pandoc details.yml -o cv_academic.tex --template=templates/academic.tex --latex-engine=xelatex
-pandoc details.yml -o cv_casual.tex --template=templates/casual.tex --latex-engine=xelatex
+pandoc details.yml -o ./cv_academic.tex --template=./templates/academic.tex --latex-engine=xelatex
+pandoc details.yml -o ./cv_casual.tex --template=./templates/casual.tex --latex-engine=xelatex
 latexmk -pdf
 latexmk -c
 rm cv_academic.tex
 rm cv_casual.tex
+rm *.bbl
+rm *.run.xml
