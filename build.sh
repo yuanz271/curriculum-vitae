14fb2960c25129b0b63201f91ba34a0a38fa5273
yuan <yuanz271@gmail.com>
2017-10-02 19:12:18 -0400

    font doesn't support xelatex option of latexmk

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 for f in templates/*.tex; do
-    pandoc details.yml -o cv_$(basename $f) --template=$f --latex-engine=xelatex
+    pandoc details.yml -o $(basename $f) --template=$f --latex-engine=xelatex
 done
-# pandoc details.yml -o ./cv_academic.tex --template=./templates/academic.tex --latex-engine=xelatex
-# pandoc details.yml -o ./cv_casual.tex --template=./templates/casual.tex --latex-engine=xelatex
 latexmk -pdf
 latexmk -c
 rm *.tex
