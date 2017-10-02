de9ed0b3db0bd797175c5da2fbe545a8a39c1ce2
yuan <yuanz271@gmail.com>
2017-10-02 10:08:46 -0400

    build: build all templates

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-pandoc details.yml -o ./cv_academic.tex --template=./templates/academic.tex --latex-engine=xelatex
-pandoc details.yml -o ./cv_casual.tex --template=./templates/casual.tex --latex-engine=xelatex
+for f in templates/*.tex; do
+    echo $f
+    pandoc details.yml -o cv_$(basename $f) --template=$f --latex-engine=xelatex
+done
+# pandoc details.yml -o ./cv_academic.tex --template=./templates/academic.tex --latex-engine=xelatex
+# pandoc details.yml -o ./cv_casual.tex --template=./templates/casual.tex --latex-engine=xelatex
 latexmk -pdf
 latexmk -c
-rm cv_academic.tex
-rm cv_casual.tex
+rm *.tex
 rm *.bbl
 rm *.run.xml
