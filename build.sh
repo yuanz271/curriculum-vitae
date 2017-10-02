dc7675edbd38ce02c4a4a9a666aa8ec70fe6d5ec
yuan <yuanz271@gmail.com>
2017-10-02 13:16:08 -0400

    bib: add subgroup paper

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 for f in templates/*.tex; do
-    echo $f
     pandoc details.yml -o cv_$(basename $f) --template=$f --latex-engine=xelatex
 done
 # pandoc details.yml -o ./cv_academic.tex --template=./templates/academic.tex --latex-engine=xelatex
