35958434bf8e36a20aa7c9e47634d4c38820aa0f
yuan <yuanz271@gmail.com>
2017-10-02 21:53:21 -0400

    fix: xelatex

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 for f in templates/*.tex; do
     pandoc details.yml -o $(basename $f) --template=$f --latex-engine=xelatex
 done
-latexmk -pdf
+latexmk -xelatex
 latexmk -c
-rm *.tex
+# rm *.tex
 rm *.bbl
 rm *.run.xml
+rm *.xdv
