980700e64a4cb6fa8edc800f3efd1b2f9ba4a3bd
yuan <yuanz271@gmail.com>
2017-10-26 10:51:29 -0400

    upload all pdf

--- upload.sh
+++ upload.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # A script to upload a file to dropbox
-basename=`basename $1`
+for f in ./*.pdf; do
+    basename=`basename $f`
 
-echo
-echo "Upload $basename to Dropbox"
-echo
+    echo
+    echo "Upload $basename to Dropbox"
+    echo
 
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer $DROPBOX_TOKEN" \
-    --header "Dropbox-API-Arg: {\"path\": \"/$basename\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @$1 \
-    > /dev/null
+    curl -X POST https://content.dropboxapi.com/2/files/upload \
+        --header "Authorization: Bearer $DROPBOX_TOKEN" \
+        --header "Dropbox-API-Arg: {\"path\": \"/$basename\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
+        --header "Content-Type: application/octet-stream" \
+        --data-binary @$f \
+        > /dev/null
+done
